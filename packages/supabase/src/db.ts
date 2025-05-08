71660280ef984510717e60e6a51a702d546714c2
name-of-okja <nameofokja@gmail.com>
2025-05-08 23:01:21 +0900

    Init Supabase

---
+++ packages/supabase/src/db.ts
@@ -0,0 +1,9 @@
+import { createClient } from '@supabase/supabase-js';
+import { Database } from './supabase.types';
+
+const SUPABASE_URL = process.env.SUPABASE_URL!;
+const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY!;
+
+const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+export { supabase };
